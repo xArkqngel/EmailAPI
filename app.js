f2b45e6d406a90316e02d61f6d4ca8a7e096efb5
xArkqngel <arkqngel343@gmail.com>
2023-01-17 22:08:40 -0500

    tried to set up to heroku, it is not free anymore

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const request = require('request');
+const https = require('https');
 
 const app = express();
 
@@ -12,13 +12,58 @@ app.get("/", function(req, res) {
 });
 
 app.post("/", function(req, res) {
-    var firstName = req.body.fName;
-    var lastName = req.body.lName;
-    var email = req.body.email;
+    const firstName = req.body.fName;
+    const lastName = req.body.lName;
+    const email = req.body.email;
     console.log(firstName, lastName, email);
+
+    const data = {
+        members: [
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastName
+                }
+            }
+        ]
+    };
+
+    const jsonData = JSON.stringify(data);
+    const url = "https://us9.api.mailchimp.com/3.0/lists/caacfb654e";
+    const options = {
+        method: "POST",
+        auth: "xArk:e2c0e6b83b4e802ac2f3f2d2b5177752-us9"
+    }
+    
+    
+    const request = https.request(url, options, function(response) {
+        if (response.statusCode === 200) {
+            res.sendFile(__dirname + "/success.html");
+        }
+        else {
+            res.sendFile(__dirname + "/failure.html");
+        }
+        response.on("data", function(data) {
+            console.log(JSON.parse(data));
+        })
+    })
+    
+    request.write(jsonData);
+    request.end();
 });
 
 
-app.listen(3000, function() {
+
+
+
+
+
+
+app.listen(process.env.PORT || 3000, function() {
     console.log('Example app listening on port 3000!');
 });
+
+//e2c0e6b83b4e802ac2f3f2d2b5177752-us9
+//caacfb654e.
